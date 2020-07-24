6e979df2527ea9371afc5992a10e54e147964242
Ivan Soroka <ivan.soroka@supercharge.io>
2020-07-24 19:41:04 +0200

    fix: register only one global click

--- src/app/home/screwer/screwer.component.ts
+++ src/app/home/screwer/screwer.component.ts
@@ -1,11 +1,24 @@
-import { Component, HostListener, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { windowClick$ } from '../../shared/global-events/window-click.event';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-screwer',
   templateUrl: './screwer.component.html',
   styleUrls: ['./screwer.component.scss'],
 })
-export class ScrewerComponent {
-  @HostListener('window:click')
-  onMousemove(): void {}
+export class ScrewerComponent implements OnInit, OnDestroy {
+  private readonly destroyed$ = new Subject<void>();
+
+  ngOnInit(): void {
+    windowClick$.pipe(takeUntil(this.destroyed$)).subscribe(() => {
+      console.log('click');
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
 }
